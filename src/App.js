9573fda43a252996713ad70b43a66d5c0f9c6f46
Abhinav <singh09.abhinav@gmail.com>
2017-06-15 15:06:03 +0530

    initial commit to git

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      headerText: "Welcome to React",
+      contentText: "In this lecture, we will go over the Components API"
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          /*
+          <h1>{this.state.headerText}</h1>
+          <p>{this.state.contentText}</p>
+          */
+        }
+        <Content />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <p className="App-intro">
+        <h1>Components API!</h1>
+        <p>In this lecture, we will go over the Components API .</p>
+      </p>
+    );
+  }
+}
+
+export default App;
